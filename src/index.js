4ee9edd0f510da6478944395c93755b78fe2913b
karpblcheva <m.karpblcheva@gmail.com>
2017-04-28 18:44:06 +0300

    develop function formatText

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log($)
+let input = $("textarea");
+let output = $("output");
+
+function formatText (text) {
+    return text.trim().replace(/' '+/g, ' ').replace(/\s+/g, ' ').toLowerCase();
+}
+
+input.on("input", function() {
+    let value = input.val();
+    let result = formatText(value);
+    output.text(formatText(result));
+});
